edeecfd14888117a53ef9c8f15048a3a573f7c9f
HydroOx1d <n-esenov44@mail.ru>
2022-08-16 11:26:17 +0600

    update: documents updated

--- js/modules/documentsData.js
+++ js/modules/documentsData.js
@@ -2,92 +2,67 @@ export const documentsData = [
   {
     id: 1,
     name: {
-      en: "Certificate of state registration of a legal entity",
-      ru: "Свидетельство государственной регистрации юридического лица",
-      ch: "法人国家注册证书",
+      en: "Certificate of state registration",
+      ru: "Свидетельство о государственной регистрации",
+      ch: "国家注册证书",
     },
-    link: "https://drive.google.com/uc?export=download&amp;id=1-G7wdiBMrFYuIwQ74JOgSyhqzdheBP8C",
+    link: {
+      en: "https://drive.google.com/uc?export=download&id=1h3q5VPlDBtbD_aTS__g2kLJ-tBHSV-Hy",
+      ru: "https://drive.google.com/uc?export=download&amp;id=1-G7wdiBMrFYuIwQ74JOgSyhqzdheBP8C",
+      ch: "https://drive.google.com/uc?export=download&id=1h3q5VPlDBtbD_aTS__g2kLJ-tBHSV-Hy",
+    },
   },
   {
     id: 2,
-    name: {
-      en: "Certificate of state registration of a legal entity",
-      ru: "Свидетельство государственной регистрации юридического лица",
-      ch: "法人国家注册证书",
-    },
-    link: "https://drive.google.com/uc?export=download&id=1h3q5VPlDBtbD_aTS__g2kLJ-tBHSV-Hy",
-  },
-  {
-    id: 3,
     name: "",
     name: {
       en: "License to organize trading on the securities market",
-      ru: "Лицензия осуществление организации торговли на рынке ценных бумаг",
+      ru: "Лицензия на организацию торгов на рынке ценных бумаг",
       ch: "证券市场交易组织许可证",
     },
-    link: "https://drive.google.com/uc?export=download&id=1Vo7zBgJJQ_0hlWUjhqKb1Hotd1naUvZq",
+    link: {
+      en: "https://drive.google.com/uc?export=download&id=1LqTnk760B5iOcSr2pbN_b6WhzSvddStZ",
+      ru: "https://drive.google.com/uc?export=download&id=1Vo7zBgJJQ_0hlWUjhqKb1Hotd1naUvZq",
+      ch: "https://drive.google.com/uc?export=download&id=1LqTnk760B5iOcSr2pbN_b6WhzSvddStZ"
+    },
+  },
+  {
+    id: 3,
+    name: {
+      en: "License for depositary activity",
+      ru: "Лицензия на депозитарную деятельность",
+      ch: "托管活动许可证",
+    },
+    link: {
+      en: "https://drive.google.com/uc?export=download&id=1cgfEMTQYwSAhvbbBNHhBp7tmWApFRaLz",
+      ru: "https://drive.google.com/uc?export=download&id=1tyvB0QkEcu2gDnHd7siOt0_kO2reL9LL",
+      ch: "https://drive.google.com/uc?export=download&id=1cgfEMTQYwSAhvbbBNHhBp7tmWApFRaLz"
+    },
   },
   {
     id: 4,
     name: {
-      en: "License for the organization of trading on the securities market",
-      ru: "Лицензия на организацию торговли на рынке ценных бумаг",
-      ch: "证券市场交易组织许可证",
+      en: "Software State Registration Certificate",
+      ru: "Патент программного обеспечения",
+      ch: "软件国家注册证",
     },
-    link: "https://drive.google.com/uc?export=download&id=1LqTnk760B5iOcSr2pbN_b6WhzSvddStZ",
+    link: {
+      en: "https://drive.google.com/uc?export=download&id=1cuQdMlxyNvwWZ839FKS5JBoC_sJ1kFdQ",
+      ru: "https://drive.google.com/uc?export=download&id=14_XrRvr572qW-R2O24fIypa3e6j_b5xE",
+      ch: "https://drive.google.com/uc?export=download&id=1cuQdMlxyNvwWZ839FKS5JBoC_sJ1kFdQ"
+    },
   },
   {
     id: 5,
     name: {
-      en: "License to carry out depository activities in the securities market",
-      ru: "Лицензия на осуществление депозитарной деятельности на рынке ценных бумаг",
-      ch: "在证券市场开展存管业务的许可证",
+      en: "Decision on the founding issue of shares",
+      ru: "Решение об учредительном выпуске акций",
+      ch: "关于创始发行股份的决定",
     },
-    link: "https://drive.google.com/uc?export=download&id=1tyvB0QkEcu2gDnHd7siOt0_kO2reL9LL",
-  },
-  {
-    id: 6,
-    name: {
-      en: "License for depositary activity on the securities market",
-      ru: "Лицензия на осуществление депозитарной деятельности на рынке ценных бумаг",
-      ch: "证券市场存管业务许可证",
+    link: {
+      en: "https://drive.google.com/uc?export=download&id=1q2HHa6dPSmE8518DUIMtF3Cicns3EbuP",
+      ru: "https://drive.google.com/uc?export=download&id=1sGBJr5IGt0gNS1H9gEtlCVYPyKEoL5bH",
+      ch: "https://drive.google.com/uc?export=download&id=1q2HHa6dPSmE8518DUIMtF3Cicns3EbuP"
     },
-    link: "https://drive.google.com/uc?export=download&id=1cgfEMTQYwSAhvbbBNHhBp7tmWApFRaLz",
-  },
-  {
-    id: 7,
-    name: {
-      en: "Certificate of state registration of computer program",
-      ru: "Свидетельство о государственной регистрации программы для ЭВМ",
-      ch: "计算机程序国家注册证书",
-    },
-    link: "https://drive.google.com/uc?export=download&id=14_XrRvr572qW-R2O24fIypa3e6j_b5xE",
-  },
-  {
-    id: 8,
-    name: {
-      en: "Certificate of state registration of computer program",
-      ru: "Свидетельство о государственной регистрации программы для ЭВМ",
-      ch: "计算机程序国家注册证书",
-    },
-    link: "https://drive.google.com/uc?export=download&id=1cuQdMlxyNvwWZ839FKS5JBoC_sJ1kFdQ",
-  },
-  {
-    id: 9,
-    name: {
-      en: "State service for financial market regulation and supervision under the ministry of economy and finance of the kyrgyz republic",
-      ru: "Государственная служба по регулированию и надзору финансового рынка при министерстве экономики и финансов Кыргызской Республики",
-      ch: "吉尔吉斯共和国经济和财政部下属国家金融市场监管局",
-    },
-    link: "https://drive.google.com/uc?export=download&id=1sGBJr5IGt0gNS1H9gEtlCVYPyKEoL5bH",
-  },
-  {
-    id: 10,
-    name: {
-      en: "State service for financial market regulation and supervision under the ministry of economy and finance of the kyrgyz republic",
-      ru: "Государственная служба по регулированию и надзору финансового рынка при министерстве экономики и финансов Кыргызской Республики",
-      ch: "吉尔吉斯共和国经济和财政部下属国家金融市场监管局",
-    },
-    link: "https://drive.google.com/uc?export=download&id=1q2HHa6dPSmE8518DUIMtF3Cicns3EbuP",
   },
 ];
--- js/script.js
+++ js/script.js
@@ -168,7 +168,7 @@ function sliceText(str) {
   if (typeof str !== 'string') {
     throw new Error('Not string')
   }
-  const maxLength = 40;
+  const maxLength = 30;
   const resultString = [];
   const sliceParamString = str.split(" ");
   let i = 0;
@@ -185,62 +185,75 @@ function sliceText(str) {
   return str;
 }
 
-const cols = document.querySelectorAll('.document__col');
-const colTitles = document.querySelectorAll(".document__col-title");
-
-const cache = new Map();
-
-cols.forEach((col, i) => {
-
-
-  if (!cache.has(i)) {
-    cache.set(i, colTitles[i].innerHTML);
-  }
-
-  col.addEventListener('mouseenter', () => {
-    colTitles[i].innerHTML = cache.get(i);
-    colTitles[i].classList.add('_is-hovered')
-  })
-
-  col.addEventListener('mouseleave', () => {
-    colTitles[i].innerHTML = sliceText(cache.get(i));
-    colTitles[i].classList.remove('_is-hovered')
-  })
-
-
-  colTitles[i].innerHTML = sliceText(colTitles[i].innerHTML);
-
-})
-
 function renderDocuments(lang) {
   const documentsList = document?.querySelector(".documents__list");
+  const documentContent = document?.querySelector('.document__content');
 
   if (documentsList?.children.length > 0) {
     documentsList.innerHTML = "";
   }
-
-  if (!documentsList) {
-    return false;
+  if (documentContent?.children.length > 0) {
+    documentContent.innerHTML = "";
   }
 
-  documentsData.forEach((document) => {
+  documentsData.forEach((document, index) => {
     let documentMarkup = `
-              <article class="documents__item item-document">
-                <div class="item-document__title-block">
-                  <div class="item-document__title">${document.name[lang]}</div>
+              <div class="document__col">
+                <div class="document__col-iblock">
+                  <img src="./img/documents-img/doc-img.jpg" alt="" class="document__col-img">
+                  <a href="${document.link[lang]}" download class="document__col-button">
+                    <svg class="document__col-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M19 10H20C20.2652 10 20.5196 10.1054 20.7071 10.2929C20.8946 10.4804 21 10.7348 21 11V20C21 20.2652 20.8946 20.5196 20.7071 20.7071C20.5196 20.8946 20.2652 21 20 21H4C3.73478 21 3.48043 20.8946 3.29289 20.7071C3.10536 20.5196 3 20.2652 3 20V11C3 10.7348 3.10536 10.4804 3.29289 10.2929C3.48043 10.1054 3.73478 10 4 10H5"
+                        stroke="current" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                      <path d="M15 14L12 17L9 14" stroke="current" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round"/>
+                      <path d="M15 9L12 12L9 9" stroke="current" stroke-width="2" stroke-linecap="round"
+                            stroke-linejoin="round"/>
+                      <path d="M12 12V3" stroke="current" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                  </a>
                 </div>
-                <a
-                  href="${document.link}"
-                  download
-                  target="_blank"
-                  class="item-document__download"
-                  ><img
-                    src="./img/documents-img/download.svg"
-                    alt="download_file" />
-                  <span class="item-document__download-text">Download</span
-                ></a>
-              </article>
+                <h2 class="document__col-title">${document.name[lang]}</h2>
+              </div>
     `;
-    documentsList.insertAdjacentHTML("beforeend", documentMarkup);
+    documentsList?.insertAdjacentHTML("beforeend", documentMarkup);
+    if (index < 4) {
+      documentContent?.insertAdjacentHTML("beforeend", documentMarkup)
+    }
   });
+
+  documentsCutTitle()
 }
+
+function documentsCutTitle() {
+  const cols = document.querySelectorAll('.document__col');
+  const colTitles = document.querySelectorAll(".document__col-title");
+  const cache = new Map();
+
+  cols.forEach((col, i) => {
+
+
+    if (!cache.has(i)) {
+      cache.set(i, colTitles[i].innerHTML);
+    }
+
+    col.addEventListener('mouseenter', () => {
+      colTitles[i].innerHTML = cache.get(i);
+      if (colTitles[i].innerHTML.length >= 40) {
+        colTitles[i].classList.add('_is-hovered')
+      }
+    })
+
+    col.addEventListener('mouseleave', () => {
+      colTitles[i].innerHTML = sliceText(cache.get(i));
+      colTitles[i].classList.remove('_is-hovered')
+    })
+
+
+    colTitles[i].innerHTML = sliceText(colTitles[i].innerHTML);
+
+  })
+}
+
+documentsCutTitle()
